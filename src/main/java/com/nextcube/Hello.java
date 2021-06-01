436054a32ffaa654e3d0df93f0471e3439aa63fd
Kelvin.Huang <Kelvin.Huang@next-cube.com>
2021-06-02 02:16:49 +0800

    V1

--- src/main/java/com/nextcube/Hello.java
+++ src/main/java/com/nextcube/Hello.java
@@ -5,14 +5,5 @@ public class Hello {
         //System.out.println("Hello, World!");
         PrintHello pH = new PrintHello();
         pH.print();
-        int age = 16;
-        Integer age1 = 19;
-        char a = 'm';
-        byte b = 120;
-        float weight = 66.5f;
-        float height = 1.7f;
-        boolean adult = true;
-        boolean enroll = true;
-        String name = "Kelvin";
     }
 }
